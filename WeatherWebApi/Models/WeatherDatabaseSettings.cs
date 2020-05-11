7daa41bfe49d409e72a75f5674fcf74e2fe43591
Nikolaj Tam Binh Huynh <au591235@uni.au.dk>
2020-05-11 18:45:08 +0200

    added WeatherDatabaseSettings

---
+++ WeatherWebApi/Models/WeatherDatabaseSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherWebApi.Data;
+
+namespace WeatherWebApi.Models
+{
+    public class WeatherDatabaseSettings : IWeatherDatabaseSettings
+    {
+        public string WeatherStationCollectionName { get; set; }
+        public string UserCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string  DatabaseName { get; set; }
+    }
+
+
+
+
+    public interface IWeatherDatabaseSettings
+    {
+        string WeatherStationCollectionName { get; set; }
+        string UserCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+
+    }
+}
