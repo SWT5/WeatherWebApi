08eee9c4ba6e842d6967af2310c4b5f0bd85228e
Mads Bo Mikkelsen <au615041@uni.au.dk>
2020-05-11 18:05:21 +0200

    added DateController

---
+++ WeatherWebApi/Controllers/DateController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WeatherWebApi.Services;
+
+namespace WeatherWebApi.Controllers
+{
+    public class DateController : ControllerBase
+    {
+	    private readonly WeatherStationCrud.IWeatherStationCrud _service;
+
+	    public DateController(WeatherStationCrud.IWeatherStationCrud service)
+	    {
+		    _service = service;
+	    }
+
+	    [HttpGet("{id}")]
+	    public IEnumerable<WeatherObservations> Get(DateTime id)
+	    {
+		    return _service.Get()
+			    .Where(wo => wo.TimeStamp.Date.CompareTo(id.Date) == 0)
+			    .ToList();
+	    }
+    }
+}
