9f02bfaf8f5a5f1ab79226460af0687dabc922bb
Quynh Trang Mac <201810269@post.au.dk>
2020-05-11 17:46:09 +0200

    Added services

--- WeatherWebApi/Services/WeatherStationCrud.cs
+++ WeatherWebApi/Services/WeatherStationCrud.cs
@@ -2,7 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using MongoDB.Driver;
+using WeatherWebApi.Models;
+using WeatherWebApi.Data;
 
 namespace WeatherWebApi.Services
 {
@@ -18,6 +22,37 @@ namespace WeatherWebApi.Services
             void Remove(string id);
         }
 
-        public class WeatherStationCrud
+        public class WeatherStationCrud : IWeatherForecast
+        {
+            private readonly IMongoCollection<WeatherForecast> _weatherForecast;
+            public WeatherStationCrud(IWeatherStationDBSettings settings)
+            {
+                var client = new MongoClient(settings.connectionString);
+                var database = client.GetDatabase(settings.DBName);
+
+                _weatherForecast = database.GetCollection<WeatherForecast>(settings.weatherStationCollection);
+            }
+
+            public List<WeatherForecast> Get() =>
+                _weatherForecast.Find(book => true).ToList();
+
+            public WeatherForecast Get(string id) =>
+                _weatherForecast.Find<WeatherForecast>(wo => wo. == id).FirstOrDefault();
+
+            public WeatherObservation Create(WeatherObservation book)
+            {
+                _weatherObservations.InsertOne(book);
+                return book;
+            }
+
+            public void Update(string id, WeatherObservation weatherObservation) =>
+                _weatherObservations.ReplaceOne(wo => wo.ID == id, weatherObservation);
+
+            public void Remove(WeatherObservation weatherObservation) =>
+                _weatherObservations.DeleteOne(wo => wo.ID == weatherObservation.ID);
+
+            public void Remove(string id) =>
+                _weatherObservations.DeleteOne(wo => wo.ID == id);
+        }
     }
 }
