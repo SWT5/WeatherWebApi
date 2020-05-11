c0cf66c5afdbb623a33d974231936e91bd915b12
Quynh Trang Mac <201810269@post.au.dk>
2020-05-11 17:55:25 +0200

    Crud done

--- WeatherWebApi/Services/WeatherStationCrud.cs
+++ WeatherWebApi/Services/WeatherStationCrud.cs
@@ -8,49 +8,47 @@ using WeatherWebApi.Data;
 
 namespace WeatherWebApi.Services
 {
-    public class WeatherStationCrud
+
+    public interface IWeatherStationCrud
     {
-        public interface IWeatherStationCrud
+        List<WeatherForecast> Get();
+        WeatherForecast Get(string id);
+        WeatherForecast Create(WeatherForecast book);
+        void Update(string id, WeatherForecast weatherForecast);
+        void Remove(WeatherForecast weatherForecast);
+        void Remove(string id);
+    }
+
+    public class WeatherStationCrud : IWeatherStationCrud
+    {
+        private readonly IMongoCollection<WeatherForecast> _weatherForecast;
+        public WeatherStationCrud(IWeatherStationDBSettings settings)
         {
-            List<WeatherForecast> Get();
-            WeatherForecast Get(string id);
-            WeatherForecast Create(WeatherForecast book);
-            void Update(string id, WeatherForecast weatherForecast);
-            void Remove(WeatherForecast weatherForecast);
-            void Remove(string id);
+            var client = new MongoClient(settings.connectionString);
+            var database = client.GetDatabase(settings.DBName);
+
+            _weatherForecast = database.GetCollection<WeatherForecast>(settings.weatherStationCollection);
         }
 
-        public class WeatherStationCrud : IWeatherForecast
+        public List<WeatherForecast> Get() =>
+            _weatherForecast.Find(book => true).ToList();
+
+        public WeatherForecast Get(string id) =>
+            _weatherForecast.Find<WeatherForecast>(wo => wo.Id == id).FirstOrDefault();
+
+        public WeatherForecast Create(WeatherForecast book)
         {
-            private readonly IMongoCollection<WeatherForecast> _weatherForecast;
-            public WeatherStationCrud(IWeatherStationDBSettings settings)
-            {
-                var client = new MongoClient(settings.connectionString);
-                var database = client.GetDatabase(settings.DBName);
-
-                _weatherForecast = database.GetCollection<WeatherForecast>(settings.weatherStationCollection);
-            }
-
-            public List<WeatherForecast> Get() =>
-                _weatherForecast.Find(book => true).ToList();
-
-            public WeatherForecast Get(string id) =>
-                _weatherForecast.Find<WeatherForecast>(wo => wo.Id == id).FirstOrDefault();
-
-            public WeatherForecast Create(WeatherForecast book)
-            {
-                _weatherForecast.InsertOne(book);
-                return book;
-            }
-
-            public void Update(string id, WeatherForecast weatherForecast) =>
-                _weatherForecast.ReplaceOne(wo => wo.Id == id, weatherForecast);
-
-            public void Remove(WeatherForecast weatherForecast) =>
-                _weatherForecast.DeleteOne(wo => wo.Id == weatherForecast.Id);
-
-            public void Remove(string id) =>
-                _weatherForecast.DeleteOne(wo => wo.Id == id);
+            _weatherForecast.InsertOne(book);
+            return book;
         }
+
+        public void Update(string id, WeatherForecast weatherForecast) =>
+            _weatherForecast.ReplaceOne(wo => wo.Id == id, weatherForecast);
+
+        public void Remove(WeatherForecast weatherForecast) =>
+            _weatherForecast.DeleteOne(wo => wo.Id == weatherForecast.Id);
+
+        public void Remove(string id) =>
+            _weatherForecast.DeleteOne(wo => wo.Id == id);
     }
 }
