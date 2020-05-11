2d4b1c57f7c18cf3b546eb29844479961bea4ea5
Thanh Tieu Pham <au479218@uni.au.dk>
2020-05-11 18:54:06 +0200

    made test project

---
+++ WeatherWebApi.Test/DateControllerTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace WeatherWebApi.Test
+{
+    public class DateControllerTest
+    {
+        [Fact]
+        public void ForecastResultDate()
+        {
+
+        }
+    }
+}
